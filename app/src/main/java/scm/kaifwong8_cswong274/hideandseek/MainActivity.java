520315dc0f94d52f9d51e14ff009446b83f5e78a
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-11-24 22:17:31 +0800

    v 0.0.1

---
+++ app/src/main/java/scm/kaifwong8_cswong274/hideandseek/MainActivity.java
@@ -0,0 +1,14 @@
+package scm.kaifwong8_cswong274.hideandseek;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
