2114443054fca7d3e5c8b019c914a33751faa674
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-11-24 22:32:19 +0800

    v 0.0.1 map ok

--- app/src/main/java/scm/kaifwong8_cswong274/hideandseek/MainActivity.java
+++ app/src/main/java/scm/kaifwong8_cswong274/hideandseek/MainActivity.java
@@ -2,7 +2,9 @@ package scm.kaifwong8_cswong274.hideandseek;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_map = findViewById(R.id.btn_map);
+        btn_map.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, MapsActivity.class);
+            startActivity(i);
+        });
+
     }
+
+
 }
