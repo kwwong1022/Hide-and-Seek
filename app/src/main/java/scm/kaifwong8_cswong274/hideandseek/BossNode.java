422b8d3b442a71efd11b593bee8933365e558f88
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-11-29 18:20:46 +0800

    v 0.1.0

---
+++ app/src/main/java/scm/kaifwong8_cswong274/hideandseek/BossNode.java
@@ -0,0 +1,19 @@
+package scm.kaifwong8_cswong274.hideandseek;
+
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.ar.sceneform.ux.TransformationSystem;
+
+public class BossNode extends TransformableNode {
+    private static final String TAG = "BossNode";
+
+    public BossNode(TransformationSystem transformationSystem) {
+        super(transformationSystem);
+    }
+
+    @Override
+    public void onUpdate(FrameTime frameTime) {
+        super.onUpdate(frameTime);
+
+    }
+}
---
+++ app/src/main/java/scm/kaifwong8_cswong274/hideandseek/HintNode.java
@@ -0,0 +1,19 @@
+package scm.kaifwong8_cswong274.hideandseek;
+
+import com.google.ar.sceneform.FrameTime;
+import com.google.ar.sceneform.ux.TransformableNode;
+import com.google.ar.sceneform.ux.TransformationSystem;
+
+public class HintNode extends TransformableNode {
+    private static final String TAG = "HintNode";
+
+    public HintNode(TransformationSystem transformationSystem) {
+        super(transformationSystem);
+    }
+
+    @Override
+    public void onUpdate(FrameTime frameTime) {
+        super.onUpdate(frameTime);
+
+    }
+}
