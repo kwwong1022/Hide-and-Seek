2a87b2e1067baea3d41502e83072e0c9079df786
Kw Wong <67175089+kwwong1022@users.noreply.github.com>
2021-12-05 02:59:45 +0800

    v 0.1.3

--- app/src/main/java/scm/kaifwong8_cswong274/hideandseek/MainActivity.java
+++ app/src/main/java/scm/kaifwong8_cswong274/hideandseek/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
